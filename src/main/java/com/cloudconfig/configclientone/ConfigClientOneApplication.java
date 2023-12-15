8b2c090a06b1b0e608ffb219183a9283cdae499b
vinoth.narayanan <vinoth.narayanan@users.noreply.github.com>
2023-12-15 07:52:57 +0000

    Add local repo

---
+++ src/main/java/com/cloudconfig/configclientone/ConfigClientOneApplication.java
@@ -0,0 +1,13 @@
+package com.cloudconfig.configclientone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudconfig/configclientone/ConfigClientOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudconfig.configclientone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
